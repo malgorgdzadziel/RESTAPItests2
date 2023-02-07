90e1479167e3b524156135326e3fb60e601a5656
gosia <malgorgrzadziel@o2.pl>
2023-02-07 17:26:10 +0100

    First test added

--- src/test/java/GetUserTest.java
+++ src/test/java/GetUserTest.java
@@ -1,2 +1,23 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static  org.hamcrest.Matchers.*;
+
+import static io.restassured.RestAssured.*;
 public class GetUserTest {
+
+
+    @Test
+    public void getTest(){
+
+                 given().
+                when().
+                get("https://reqres.in/api/users").
+                then().log().all();
+
+    }
+
+
+
 }
